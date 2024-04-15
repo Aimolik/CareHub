f260c1bc0bb8689942ac2f76beff0ba958fcbf21
stea10 <119765400+stea10@users.noreply.github.com>
2024-04-14 20:43:27 -0400

    Delete CareHubFrontend directory

--- CareHubFrontend/ApiRoutes.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'http://localhost:8080';
-
-export const GUARDIAN_ROUTE = BASE_URL + '/api/guardian';
-export const ACCOUNT_ROUTE = BASE_URL + '/api/account';
-export const CHILD_ROUTE = BASE_URL + '/api/child'
-export const STAFF_ROUTE = BASE_URL + '/api/staff';
-export const CHECKING_ROUTE = BASE_URL + '/api/checking';
-
-export const AUTHENTICATE_ROUTE = ACCOUNT_ROUTE + '/authenticate'
